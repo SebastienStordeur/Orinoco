b2cd5dfab92c66d57ab6f2129f7ca5b0c112c9d4
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-07-16 20:06:34 +0200

    all page product now working

--- js/productpage.js
+++ js/productpage.js
@@ -1,28 +1,31 @@
 const searchInURL = window.location.search;
 const getId = new URLSearchParams(searchInURL)
 const id = getId.get("id");
-console.log(id);
 
-function getProduct() {
+//get specific product datas + assign
+
     fetch(`http://localhost:3000/api/cameras/${id}`, {method: 'GET'})
         .then((response) => {
             response.json().then((data) => {
-                console.log(data)
+                function assignDatas(data) {
+                    object = data;
+                    const photoContainer = document.querySelector('.product__photo-container');
+                    const productTitle = document.querySelector('.product__details--title');
+                    const productDescription = document.querySelector('.product__details--description');
+                    const productPrice = document.querySelector('.product__details--price');
+                    const selector = document.querySelector('.select-lense');
+
+                    photoContainer.innerHTML = `<img src="${object.imageUrl}" alt="${object.name}"/>`;
+                    productTitle.innerHTML = object.name;
+                    productDescription.innerHTML = `${object.description}`;
+                    productPrice.innerHTML = object.price/100 +'€';
+                    
+                    for(let lense of object.lenses) {
+                        const choice = document.createElement('option');
+                        selector.appendChild(choice);
+                        choice.innerHTML = lense;
+                    }
+                }
+                assignDatas(data);
             })
         })
-        .then((selectedItm) => {
-            object = selectedItm;
-            const photoContainer = document.querySelector('.product__photo-container');
-            const productTitle = document.querySelector('.product__details--title');
-            const productDescription = document.querySelector('.product__details--description');
-            const productPrice = document.querySelector('.product__details--price');
-
-            //photoContainer.innerHTML = `<img src="${object.imageUrl}" alt="${object.name}"/>`;
-            productTitle.innerHTML = object.name;
-            //productDescription.innerHTML = `${object.description}`;
-            console.log(object)
-        })
-}
-
-//get specific product datas + assign
-getProduct();
