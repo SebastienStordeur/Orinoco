a9d0d55dd3507e59082bfd1fbdf4f0f97c6a0fbc
Sebastien Stordeur <stordeursebastien0@gmail.com>
2021-07-20 09:01:35 +0200

    assign item in shopping cart + calculate total price

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,12 +1,17 @@
-var nameValue = document.querySelector('.name-input').value;
+
+
+const shoppingCart = document.querySelector('.cart__content--items');
 const lastName = document.querySelector('.lastname-input');
 const adress = document.querySelector('.adress-input');
 const phoneNumber = document.querySelector('.phonenumber-input');
 const error = document.querySelector('.error-msg');
 const form = document.querySelector('form');
 
-function formValidation() {
+
+//Validate every single input
+/* function formValidation() {
   form.addEventListener('submit', (e) => {
+    var nameValue = document.querySelector('.name-input').value;
     var letters=/^[a-zA-Z\s]+$/; //contains only letters + spaces
     form.reset();
     e.preventDefault();
@@ -14,11 +19,50 @@ function formValidation() {
       error.innerHTML = "Caractères incorrects"
     } //si valeur de nom different des caractères lettres et espace
     else {
-      alert('bonjour')
+      alert('')
     } 
     
   })
   
-}
+} */
 
-formValidation();
+//Get element from localStorage and assign them a space in the page
+//getStorageContent(data)
+
+
+//Post order
+//formValidation();
+
+fetch("http://localhost:3000/api/cameras", {method: 'GET'})
+  .then((response) => {
+    response.json().then((data) => {
+      //Get element from localStorage
+      //getStorageContent(data);
+      //Create element for each element in storage
+      let cartItms = JSON.parse(localStorage.getItem("Cart"));
+      let totalPrice = 0;
+      cartItms.forEach( (data) => {
+        const createDiv = document.createElement("div");
+        const itemImg = document.createElement("div");
+        const itemName = document.createElement("h2");
+        const price = document.createElement("span");
+
+        shoppingCart.appendChild(createDiv);
+        createDiv.appendChild(itemImg);
+        createDiv.appendChild(itemName);
+        createDiv.appendChild(price);
+
+        createDiv.classList.add('cart__content--item');
+        itemImg.classList.add('cart__content--item__img');
+        price.classList.add('cart__content--item__price');
+
+        itemImg.innerHTML = `<img src="${data.imageUrl}"/>`;
+        itemName.innerHTML = data.name;
+        price.innerHTML = `<h3>${data.price/100 + "€"}</h3>`;
+        totalPrice += data.price/100;
+        console.log(totalPrice);
+      })
+    
+    document.querySelector('.total-price').innerHTML = totalPrice + "€";
+    })
+  })
--- js/storage.js
+++ js/storage.js
@@ -1,32 +1,4 @@
-/* //Local Storage function
-function storage(data) {
-  document.querySelector('.product__addCart').addEventListener('click', (e) => {
-    e.preventDefault();
-    if('localStorage' in window && window['localStorage'] !== null) {
-      try {
-        //get data stored 
-        var cart = JSON.parse(localStorage.getItem(data));
-        if (cart === null) {
-          //if nothing is stored, then create an empty array
-          cart=[];
-        }
-        //Create new entry
-        cartItm = {
-          id: data._id.val(),
-          name: data._name.val()
-        };
-        //add new entry to stored array
-        cart.push(cartItm);
-        //sava array
-        localStorage.setItem("cart", JSON.stringify(cart));
-      }
-      catch(e) {
-        console.log("erreur");
-      }
-    }
-  })
-} */
-  
+
 function storage(data) {
   document.querySelector('.product__addCart').addEventListener('click', (e) => {
     e.preventDefault();
@@ -52,6 +24,5 @@ function storage(data) {
   let cartItms = JSON.parse(localStorage.getItem("Cart"));
   if (!cartItms) return ;
   else data = cartItms;
-  //console.log(cartItms)
 } 
 
